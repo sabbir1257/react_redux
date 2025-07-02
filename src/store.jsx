193fb018c03157e3a53be4fbfbf0d4fb46dcf9cd
sabbir1257 <masabbir1257@gmail.com>
2025-07-02 11:49:24 +0600

    first commit

---
+++ src/store.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-case-declarations */
+const ADD_TASK = 'task/add';
+const DELETE_TASK = 'task/delete';
+
+const initialState = {
+  task: [],
+};
+
+const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        task: [...state.task, action.payload],
+      };
+
+    case DELETE_TASK:
+      const updatedTask = state.task.filter((curTask, index) => {
+        return index !== action.payload;
+      });
+      return {
+        ...state,
+        task: [...state.task, updatedTask],
+      };
+
+    default:
+        return state;
+  }
+};
